36257e70415ea430bfd4163244535fde750d9242
Rainbow <rainbow_weic@163.com>
2019-08-15 17:47:23 +0800

    移动端逻辑处理

--- computer/js/index.js
+++ computer/js/index.js
@@ -1,106 +1,156 @@
 $(function(){
-  //弹窗的隐藏
- $(".alert-close").click(function(){
-  cancel();
+
+
+//弹窗的隐藏
+$("#close").click(function(){
+  cancel($("#alertBox"),$("#shelter"));
+})
+//弹窗的隐藏
+$("#close2").click(function(){
+  cancel($("#alertBox2"),$("#shelter2"));
 })
 
-function showSelectBox() {
-  $("#alertBox").css("display","block");
-  $("#shelter").css("display","block");
+function showSelectBox(alertBox,shelter) {
+  alertBox.css("display","block");
+  shelter.css("display","block");
 }
-function cancel() {
-  $("#alertBox").css("display","none");
-  $("#shelter").css("display","none");
+function cancel(alertBox,shelter) {
+  alertBox.css("display","none");
+  shelter.css("display","none");
 }
 
 
-//获得焦点
+//获得焦点清除错误提示语
 $( '.phone-from #phone' ).focus( function(){
   $(".header-error").text("");
-  })
+})
+
+$( '.phone-from2 .txt' ).focus( function(){
+  $(".error").text("");
+})
+$( '.form-code .text' ).focus( function(){
+  $(".code-error").text("");
 })
 //点击免费领取体验课
-$(".phone-from #btn").click(function(){
-  var Phone = $("#phone").val();
-  var contac = /^1[345789]\d{9}$/g;
-  var count = 5;
- if((Phone == "")){
-   $(".header-error").text("手机号码不能为空");
-  }else if(!contac.exec(Phone)){
-    $(".header-error").text("请输入正确的手机号码");
-  }else{
-    $(".header-error").text("");
-    //弹窗显示
-    // showSelectBox();
-    $("#alertBox .phone").val(Phone);
+from($(".phone-from #btn"),$("#phone"),$(".header-error"))
+//点击立即领取
+from($(".phone-from #btn2"),$("#phone2"),$(".error"))
 
-      //开始计时  
-      $(".get_captcha").attr('disabled','disabled');  
-      $(".get_captcha").html(count + "s后再次获取");  
-      var timer = setInterval(function(){  
-        count--;
-        $(".get_captcha").html( count + "s后再次获取"); 
-        if (count==0) {
-          clearInterval(timer);
-          $(".get_captcha").attr("disabled",false);//启用按钮  
-          $(".get_captcha").html("重新发送验证码");
-          $(".get_captcha").addClass('on');
-          code = "";//清除验证码。如果不清除，过时间后，输入收到的验证码依然有效 
-        }
-      },1000);
 
-        //向后台发送处理数据  
-        $.ajax({  
-          type: "get", //用POST方式传输  
-          url: 'http://t.xingxiwang.com/sendcode', //目标地址 
-          data:{
-            phone:Phone
-          } ,   
-          success: function (msg){
-            if(msg == 1){
-              showSelectBox();
-            }
-          }  
-      });
-  }
- 
-})
 
-//点击重新发送验证码
-$(".get_captcha").click(function(){
-  var Phone = $(".from .phone").val();
-    $.ajax({  
-    type: "get", //用POST方式传输  
-    url: 'http://t.xingxiwang.com/sendcode', //目标地址 
-    data:{
-      phone:Phone
+function from(clickelement,phone,error){
+  clickelement.click(function(){
+    var Phone = phone.val();
+    var contac = /^1[345789]\d{9}$/g;
+    var count = 10;
+   if((Phone == "")){
+      error.text("手机号码不能为空");
+    }else if(!contac.exec(Phone)){
+      error.text("请输入正确的手机号码");
+    }else{
+      error.text("");
+      $("#alertBox .phone").val(Phone);
+  
+        //开始计时 
+        $(".get-captcha").removeClass('on'); 
+        $(".get-captcha").attr('disabled','disabled');  
+        $(".get-captcha").html(count + "s后再次获取");  
+        var timer = setInterval(function(){  
+          count--;
+          $(".get-captcha").html( count + "s后再次获取"); 
+          if (count==0) {
+            clearInterval(timer);
+            $(".get-captcha").attr("disabled",false);//启用按钮  
+            $(".get-captcha").html("重新发送验证码");
+            $(".get-captcha").addClass('on');
+            code = "";//清除验证码。如果不清除，过时间后，输入收到的验证码依然有效
+            
+            //点击重新发送验证码
+            $(".get-captcha").click(function(){
+              count = 10;  
+              var Phone = $(".from .phone").val();
+              $(".get-captcha").removeClass('on');
+              $(".get-captcha").attr('disabled','disabled');  
+              $(".get-captcha").html(count + "s后再次获取"); 
+              var timer = setInterval(function(){
+                count--;
+                $(".get-captcha").html( count + "s后再次获取"); 
+                if (count==0) {
+                  clearInterval(timer);
+                  $(".get-captcha").attr("disabled",false);//启用按钮  
+                  $(".get-captcha").html("重新发送验证码");
+                  $(".get-captcha").addClass('on');
+                  code = "";//清除验证码。如果不清除，过时间后，输入收到的验证码依然有效
+                }      
+              },1000)
+                $.ajax({  
+                type: "get", //用POST方式传输  
+                url: 'http://t.xingxiwang.com/sendcode', //目标地址 
+                data:{
+                  phone:Phone
+                }
+              });
+            })
+          }
+        },1000);
+  
+          //向后台发送处理数据  
+          $.ajax({  
+            type: "get", //用POST方式传输  
+            url: 'http://t.xingxiwang.com/sendcode', //目标地址 
+            data:{
+              phone:Phone
+            } ,   
+            success: function (msg){
+              if(msg == 1){
+                showSelectBox( $("#alertBox"),$("#shelter"));
+              }else{
+                error.text("请您不要频繁的点击发送");
+              }
+            }  
+        });
     }
-  });
+   
+  })
+  
+}
+
+  //点击立即提交
+  $("#code-submit").click(function(){
+
+    var Phone = $(".from .phone").val();
+    var Code = $(".form-code .text").val();
+
+    var contac = /^1[345789]\d{9}$/g;
+
+    if(Code == ""){
+      $(".code-error").html("请您输入验证码！！！")
+    }else if((Phone == "")){
+      $(".code-error").text("手机号码不能为空");
+    }else if(!contac.exec(Phone)){
+      $(".code-error").text("请输入正确的手机号码");
+    }else{
+      $.ajax({  
+        type: "get", //用POST方式传输  
+        url: 'http://t.xingxiwang.com/checkcode', //目标地址 
+        data:{
+          phone:Phone,
+          code:Code
+        },  
+        success: function (msg){
+          if(msg == 1){
+            
+            showSelectBox( $("#alertBox2"),$("#shelter2"));
+            cancel($("#alertBox"),$("#shelter"));
+            $("#phone,#phone2,.form-code .text,.from .phone").val("");
+          }
+          else{
+              $(".code-error").html("您输入的正确的验证码！！！") 
+          } 
+        } 
+      });
+    }
+  })
+
 })
-
-//点击立即提交
-$("#code-submit").click(function(){
- var Phone = $(".from .phone").val();
- var Code = $(".form_code .text").val();
- if(!Code){
-  $(".code-error").html("请输入您的验证码") 
- }
-  $.ajax({  
-  type: "get", //用POST方式传输  
-  url: 'http://t.xingxiwang.com/checkcode', //目标地址 
-  data:{
-    phone:Phone,
-    code:Code
-  },  
-  success: function (msg){
-   if(msg == 1){
-     alert("提交成功")
-   }else{
-     $(".code-error").html("您输入的验证码有误！！！") 
-  } 
-} 
-});
-})
-
-
 
--- mobile/js/index.js
+++ mobile/js/index.js
@@ -1,5 +1,134 @@
 $(function(){
-  $(".from-btn").click(function(){
+
+
+  //获得焦点清除错误提示语
+$( '.from .phone,.from .text,.from .name' ).focus( function(){
+  $(".from .error").text("");
+})
+
+//弹窗的隐藏
+$("#close").click(function(){
+  cancel($("#alertBox"),$("#shelter"));
+})
+
+function showSelectBox(alertBox,shelter) {
+  alertBox.css("display","block");
+  shelter.css("display","block");
+}
+function cancel(alertBox,shelter) {
+  alertBox.css("display","none");
+  shelter.css("display","none");
+}
+
+
+  $("#from-btn").click(function(){
+    var Phone = $(".from .phone").val();
+    var Error = $(".from .error");
+    var contac = /^1[345789]\d{9}$/g;
+    var count = 10;
+    if(Phone == ""){
+      Error.text("手机号码不能为空")
+    }else if(!contac.test(Phone)){
+      Error.text("请输入正确的手机号码")
+    }else{
     
+      $(".tips").text("验证码已发送至"+ Phone );
+
+
+       //开始计时 
+       $(".get-captcha").removeClass('on'); 
+       $(".get-captcha").attr('disabled','disabled');  
+       $(".get-captcha").html(count + "s后再次获取");  
+       var timer = setInterval(function(){  
+         count--;
+         $(".get-captcha").html( count + "s后再次获取"); 
+         if (count==0) {
+           clearInterval(timer);
+           $(".get-captcha").attr("disabled",false);//启用按钮  
+           $(".get-captcha").html("重新发送验证码");
+           $(".get-captcha").addClass('on');
+           code = "";//清除验证码。如果不清除，过时间后，输入收到的验证码依然有效
+           
+           //点击重新发送验证码
+           $(".get-captcha").click(function(){
+             count = 10;  
+             var Phone = $(".from .phone").val();
+             $(".get-captcha").removeClass('on');
+             $(".get-captcha").attr('disabled','disabled');  
+             $(".get-captcha").html(count + "s后再次获取"); 
+             var timer = setInterval(function(){
+               count--;
+               $(".get-captcha").html( count + "s后再次获取"); 
+               if (count==0) {
+                 clearInterval(timer);
+                 $(".get-captcha").attr("disabled",false);//启用按钮  
+                 $(".get-captcha").html("重新发送验证码");
+                 $(".get-captcha").addClass('on');
+                 code = "";//清除验证码。如果不清除，过时间后，输入收到的验证码依然有效
+               }      
+             },1000)
+               $.ajax({  
+               type: "get", //用POST方式传输  
+               url: 'http://t.xingxiwang.com/sendcode', //目标地址 
+               data:{
+                 phone:Phone
+               }
+             });
+           })
+         }
+       },1000);
+         //向后台发送处理数据  
+         $.ajax({  
+          type: "get", //用POST方式传输  
+          url: 'http://t.xingxiwang.com/sendcode', //目标地址 
+          data:{
+            phone:Phone
+          } ,   
+          success: function (msg){
+            if(msg == 1){
+              $(".form-phone").css("display","none");
+              $(".form-code").css("display","block");
+            }
+          }  
+      });
+    }
+  })
+
+
+
+  //点击立即领取
+  $("#from-btn2").click(function(){
+
+    var Phone = $(".from .phone").val();
+    var Code = $(".form-code .text").val();
+    var Name = $(".form-name .name").val();
+
+    console.log(Name,"+++++++")
+    if(Code == ""){
+      $(".error").text("请您输入验证码！！！")
+    }else if(Name == ""){
+      $(".error").text("宝贝名字不能为空");
+    }else{
+      $.ajax({  
+        type: "get", //用POST方式传输  
+        url: 'http://t.xingxiwang.com/checkcode', //目标地址 
+        data:{
+          phone:Phone,
+          code:Code
+        },  
+        success: function (msg){
+          if(msg == 1){
+            
+            showSelectBox($("#alertBox"),$("#shelter"))
+            $(".phone,.name,.text").val("");
+            $(".form-phone").css("display","block");
+            $(".form-code").css("display","none");
+          }
+          else{
+              $(".error").html("您输入的正确的验证码！！！") 
+          } 
+        } 
+      });
+    }
   })
 })
